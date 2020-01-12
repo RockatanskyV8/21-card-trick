9b9a4e15f843f95dd866cbc8dea0d43d866b6c48
RockatanskyV8 <luccasanttoz@gmail.com>
2020-01-12 16:13:45 -0300

    correção de erro

--- src/Components/ThreePiles.js
+++ src/Components/ThreePiles.js
@@ -2,7 +2,6 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 import Pile from "./Pile";
 import Loader from "./Loader";
-import davidblaine from "../images/parodydavidblaine.gif";
 
 const ThreePiles = ({ deckID }) => {
   const [cardsRemaining, setCardsRemaining] = useState(21);
